9ebab4e033b45d6af2d62960e1f55b7c8e567fdd
Nghia Le <nghia.le@careercross.com>
2024-01-14 10:05:48 +0900

    Fagi for stock market

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,169 @@
-import Image from 'next/image'
+'use client'
+
+import dynamic from "next/dynamic";
+import { useState, useEffect } from 'react'
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend,
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
+const GaugeComponent = dynamic(() => import('react-gauge-component'), { ssr: false });
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
 
 export default function Home() {
+  const [stockFagiData, setStockFagiData] = useState<any>(null)
+  const [selectedTab, setSelectedTab] = useState(0);
+
+  useEffect(() => {
+    fetch('https://production.dataviz.cnn.io/index/fearandgreed/graphdata', {
+      method: 'GET',
+      headers: {
+        // Need to set User-Agent to mimic a normal browser otherwise CNN will think this is a bot and won't provide the data
+        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Safari/537.36'
+      }
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        setStockFagiData(data)
+      })
+  }, [])
+
+  if (!stockFagiData) {
+    return <p>Loading...</p>
+  }
+
+  const stockFagiValue = Math.round(stockFagiData.fear_and_greed.score)
+  const stockFagiHistoricalDates = stockFagiData.fear_and_greed_historical.data.map(p => (new Date(p.x)).toISOString().split('T')[0]);
+  const stockFagiHistoricalValues = stockFagiData.fear_and_greed_historical.data.map(p => Math.round(p.y));
+  const explainStockFagiValue = (value: number): string => {
+    if (value < 25) {
+      return "Extreme Fear"
+    } else if (value < 45) {
+      return "Fear"
+    } else if (value < 55) {
+      return "Neutral"
+    } else if (value < 75) {
+      return "Greed"
+    } else {
+      return "Extreme Greed"
+    }
+  }
+  const stockFagiText = explainStockFagiValue(stockFagiValue)
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
+    <main className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
+      <section id="stock-fagi" className="flex flex-col items-center w-full max-w-4xl p-8 bg-white rounded-lg shadow-md">
+        <h1 className="text-4xl font-bold mb-8 text-center">Fear & Greed Index - Stock Market</h1>
+
+        <div className="flex w-full">
+          <button
+            className={`w-1/2 p-4 ${selectedTab === 0 ? 'bg-blue-500 text-white' : 'bg-gray-200 text-gray-800'}`}
+            onClick={() => setSelectedTab(0)}
           >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+            Overview
+          </button>
+          <button
+            className={`w-1/2 p-4 ${selectedTab === 1 ? 'bg-blue-500 text-white' : 'bg-gray-200 text-gray-800'}`}
+            onClick={() => setSelectedTab(1)}
+          >
+            Timeline
+          </button>
         </div>
-      </div>
 
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px] z-[-1]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+        {selectedTab === 0 && (
+          <section className="p-4">
 
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+            <GaugeComponent
+              value={stockFagiValue}
+              type="radial"
+              labels={{
+                valueLabel: {
+                  matchColorWithArc: true,
+                  formatTextValue: (value) => explainStockFagiValue(value)
+                },
+                tickLabels: {
+                  type: "outer",
+                  ticks: [
+                    { value: 0 },
+                    { value: 25 },
+                    { value: 45 },
+                    { value: 55 },
+                    { value: 75 },
+                    { value: 100 }
+                  ],
+                  defaultTickValueConfig: { formatTextValue: value => value, style: { "fontSize": "20px" } },
+                },
+              }}
+              arc={{
+                cornerRadius: 0,
+                colorArray: ['#EA4228', '#5BE12C'],
+                subArcs: [{ limit: 24 }, { limit: 44 }, { limit: 54 }, { limit: 74 }, { limit: 100 }],
+                padding: 0,
+                width: 0.3,
+                gradient: true
+              }}
+              pointer={{
+                elastic: false,
+                animationDelay: 0
+              }}
+            />
+            <p className="text-center text-xl font-semibold" style={{"color": "rgb(173, 172, 171)"}}>{stockFagiValue}</p>
+          </section>
+        )}
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+        {selectedTab === 1 && (
+          <section className="p-4 w-full">
+            <Line
+              options={{
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+                responsive: true,
+                maintainAspectRatio: true,
+                plugins: {
+                  legend: {
+                    display: false
+                  },
+                  tooltip: {
+                    callbacks: {
+                      label: (tooltipItem: any) => {
+                        return `${tooltipItem.raw} (${explainStockFagiValue(tooltipItem.raw)})`;
+                      },
+                    }
+                 }
+                },
+                // elements: {
+                //   point: {
+                //     radius: 0
+                //   }
+                // }
+              }}
+              data={{
+                labels: stockFagiHistoricalDates,
+                datasets: [
+                  {
+                    data: stockFagiHistoricalValues,
+                    borderColor: 'rgb(255, 99, 132)',
+                    backgroundColor: 'rgba(255, 99, 132, 0.5)',
+                  },
+                ],
+              }}
+            />
+          </section>
+        )}
+      </section>
     </main>
-  )
+  );
 }
--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    // prefer loading of es modules over commonjs
+    // reactStrictMode: true,
+    experimental: { esmExternals: 'loose' }
+}
 
 module.exports = nextConfig
